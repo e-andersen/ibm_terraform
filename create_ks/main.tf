e1957211b644f26920ebca66ef0ae309ddd09fb3
Erik Andersen <ean@dk.ibm.com>
2025-12-19 10:15:30 +0100

    Add AZs

--- create_ks/main.tf
+++ create_ks/main.tf
@@ -9,8 +9,8 @@ resource "ibm_container_cluster" "tfcluster" {
   datacenter      = "dal10"
   machine_type    = "b3c.4x16"
   hardware        = "shared"
-  public_vlan_id  = "2234945"
-  private_vlan_id = "2234947"
+  private_vlan_id = "3527884"
+  public_vlan_id  = "3527882"
 
   kube_version = "1.33.6"
 
@@ -19,5 +19,61 @@ resource "ibm_container_cluster" "tfcluster" {
   public_service_endpoint  = "true"
   private_service_endpoint = "true"
 
+  resource_group_id = data.ibm_resource_group.resource_group.id
+}
+
+resource "ibm_container_worker_pool_zone_attachment" "dal12" {
+  cluster           = ibm_container_cluster.tfcluster.id
+  worker_pool       = ibm_container_cluster.tfcluster.worker_pools.0.id
+  zone              = "dal12"
+  private_vlan_id   = "3527888"
+  public_vlan_id    = "3527886"
+  resource_group_id = data.ibm_resource_group.resource_group.id
+}
+
+resource "ibm_container_worker_pool_zone_attachment" "dal13" {
+  cluster           = ibm_container_cluster.tfcluster.id
+  worker_pool       = ibm_container_cluster.tfcluster.worker_pools.0.id
+  zone              = "dal13"
+  private_vlan_id   = "3527892"
+  public_vlan_id    = "3527890"
+  resource_group_id = data.ibm_resource_group.resource_group.id
+}
+
+resource "ibm_container_worker_pool" "workerpool" {
+  worker_pool_name = "tf-workerpool"
+  machine_type     = "u3c.2x4"
+  cluster          = ibm_container_cluster.tfcluster.id
+  size_per_zone    = 2
+  hardware         = "shared"
+
+  resource_group_id = data.ibm_resource_group.resource_group.id
+}
+
+resource "ibm_container_worker_pool_zone_attachment" "tfwp-dal10" {
+  cluster         = ibm_container_cluster.tfcluster.id
+  worker_pool     = element(split("/", ibm_container_worker_pool.workerpool.id), 1)
+  zone            = "dal10"
+  private_vlan_id = "3527884"
+  public_vlan_id  = "3527882"
+
+  resource_group_id = data.ibm_resource_group.resource_group.id
+}
+
+resource "ibm_container_worker_pool_zone_attachment" "tfwp-dal12" {
+  cluster           = ibm_container_cluster.tfcluster.id
+  worker_pool       = element(split("/", ibm_container_worker_pool.workerpool.id), 1)
+  zone              = "dal12"
+  private_vlan_id   = "3527888"
+  public_vlan_id    = "3527886"
+  resource_group_id = data.ibm_resource_group.resource_group.id
+}
+
+resource "ibm_container_worker_pool_zone_attachment" "tfwp-dal13" {
+  cluster           = ibm_container_cluster.tfcluster.id
+  worker_pool       = element(split("/", ibm_container_worker_pool.workerpool.id), 1)
+  zone              = "dal13"
+  private_vlan_id   = "3527892"
+  public_vlan_id    = "3527890"
   resource_group_id = data.ibm_resource_group.resource_group.id
 }
